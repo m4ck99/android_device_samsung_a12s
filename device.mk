2ae159d25680452147859b75376762c2147be73f
Tim Zimmermann <tim@linux4.de>
2021-11-29 20:54:35 +0100

    m12: enable fingerprint gestures
    Change-Id: I3289fac865b8d7bf5900d8fa8b3f5b03b818a230

--- device.mk
+++ device.mk
@@ -20,5 +20,9 @@ $(call inherit-product, device/samsung/exynos850-common/common.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# Fingerprint Gestures
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/uinput-sec-fp.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-sec-fp.kl
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
