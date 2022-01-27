4608f337282f4b2931e5f90d5863c8fe921d9bcb
ActuallyNotGelb <g3lb3r.fuser@gmail.com>
2022-01-27 16:00:21 +0100

    m12: initial commit
    Change-Id: Id6a5fd876ec5739fb3405330861f9697781b3404

---
+++ BoardConfig.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## Inherit from the common tree
+include device/samsung/exynos850-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/m12
+
+## APEX image
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
+## Display
+TARGET_SCREEN_DENSITY := 300
+
+## Kernel
+TARGET_KERNEL_CONFIG := exynos850-m12nsxx_defconfig
---
+++ lineage_m12.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+## Inherit from generic products, most specific first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+## Inherit from m12 device
+$(call inherit-product, device/samsung/m12/device.mk)
+
+## Boot Animation
+TARGET_SCREEN_HEIGHT := 1600
+TARGET_SCREEN_WIDTH := 720
+
+## Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+## Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+## Device identifier, this must come after all inclusions
+PRODUCT_NAME := lineage_m12
+PRODUCT_DEVICE := m12
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M127F
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
