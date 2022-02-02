5841acd758d948f6a1f5b64ae3735c8a6083890c
ActuallyNotGelb <g3lb3r.fuser@gmail.com>
2022-02-02 17:25:13 +0100

    m12: Import proprietary file extraction scripts
    Change-Id: Ic633b911bd0542f36f5660c8a61d9caf14f0835c

---
+++ setup-makefiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=m12
+export DEVICE_COMMON=exynos850-common
+export VENDOR=samsung
+
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
